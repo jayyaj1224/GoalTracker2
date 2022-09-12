0ed6c54d410f6abb5e6e9a0c8a9f0474b7057d12
Jay Lee <73763976+jayyaj1224@users.noreply.github.com>
2022-09-12 13:01:38 +0900

    Initial Commit

---
+++ GoalTracker2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoalTracker2
+//
+//  Created by Jay Lee on 12/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
