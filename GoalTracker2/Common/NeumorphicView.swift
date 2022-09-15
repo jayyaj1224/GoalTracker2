ea3a0e3e265405b20acfe7afc092163a6df7feca
leejongyun <73763976+jayyaj1224@users.noreply.github.com>
2022-09-15 11:38:47 +0900

    neumorphicView

--- GoalTracker2/Common/NeumorphicView.swift
+++ GoalTracker2/Common/NeumorphicView.swift
@@ -7,45 +7,58 @@
 
 import UIKit
 
+class Neumorphic {
+    
+}
+
 /// UIView with Neumorphism Shadow
 /// *     enum NeumorphicSize { case small, medium, large }
 /// *     func setNeumorphicShadow(size shadowSize: NeumorphicSize)
 ///
 class NeumorphicView: UIView {
-
-    private let whiteUpperShadowLayer = CALayer()
+    enum ShadowSize {
+        case small, medium, large
+    }
     
+    private let whiteUpperShadowLayer = CALayer()
     private let blackUnderShadowLayer = CALayer()
     
-    init() {
+    init(shadowSize: ShadowSize) {
         super.init(frame: .zero)
         
         layer.insertSublayer(whiteUpperShadowLayer, at: 0)
         layer.insertSublayer(blackUnderShadowLayer, at: 0)
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layoutNeumorphicShadows()
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    
-    // 아래의 2가지 내용이 UIView Extension 에 들어가면 좋다
-    enum NeumorphicSize {
-        case small, medium, large
+    private func layoutNeumorphicShadows() {
+        [whiteUpperShadowLayer, blackUnderShadowLayer]
+            .forEach { layer in
+                layer.frame = self.layer.bounds
+                layer.cornerRadius = self.layer.cornerRadius
+            }
     }
-    
-    func setNeumorphicShadow(size shadowSize: NeumorphicSize) {
+
+    private func setNeumorphicShadow(shadowSize: ShadowSize, whiteShadowLayer: CALayer, blackShadowLayer: CALayer) {
         switch shadowSize {
         case .small:
-            setDropShadow(layer: whiteUpperShadowLayer, color: .black, width: 1, height: 1, blur: 3, spread: -1, opacity: 0.2)
-            setDropShadow(layer: blackUnderShadowLayer, color: .white, width: -2, height: -2, blur: 4, spread: 0, opacity: 0.9)
+            setDropShadow(layer: whiteShadowLayer, color: .black, width: 1, height: 1, blur: 3, spread: -1, opacity: 0.2)
+            setDropShadow(layer: blackShadowLayer, color: .white, width: -2, height: -2, blur: 4, spread: 0, opacity: 0.9)
         case .medium:
-            setDropShadow(layer: whiteUpperShadowLayer, color: .black, width: 2, height: 2, blur: 6, spread: -2, opacity: 0.2)
-            setDropShadow(layer: blackUnderShadowLayer, color: .white, width: -3.6, height: -3.6, blur: 8, spread: 0, opacity: 0.9)
+            setDropShadow(layer: whiteShadowLayer, color: .black, width: 2, height: 2, blur: 6, spread: -2, opacity: 0.2)
+            setDropShadow(layer: blackShadowLayer, color: .white, width: -3.6, height: -3.6, blur: 8, spread: 0, opacity: 0.9)
         case .large:
-            setDropShadow(layer: whiteUpperShadowLayer, color: .black, width: 2, height: 2, blur: 6, spread: -2, opacity: 0.2)
-            setDropShadow(layer: blackUnderShadowLayer, color: .white, width: -3.6, height: -3.6, blur: 8, spread: 0, opacity: 0.9)
+            setDropShadow(layer: whiteShadowLayer, color: .black, width: 2, height: 2, blur: 6, spread: -2, opacity: 0.2)
+            setDropShadow(layer: blackShadowLayer, color: .white, width: -3.6, height: -3.6, blur: 8, spread: 0, opacity: 0.9)
         }
     }
 }
