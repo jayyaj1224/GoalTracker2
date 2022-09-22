9259c03bb0d2f5ac9bc598c684a22c306f3a2235
Jay Lee <73763976+jayyaj1224@users.noreply.github.com>
2022-09-22 23:48:40 +0900

    0922

---
+++ GoalTracker2/Calendar/CalendarViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarViewController.swift
+//  GoalTracker2
+//
+//  Created by Jay Lee on 22/09/2022.
+//
+
+import Foundation
---
+++ GoalTracker2/Settings/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  GoalTracker2
+//
+//  Created by Jay Lee on 22/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    //MARK: - ui setting
+    private func configure() {
+        
+    }
+}
+
