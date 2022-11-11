13a6d0f9b29ea60622536fc0be711c1d8ea3402c
Jay Lee <73763976+jayyaj1224@users.noreply.github.com>
2022-11-11 21:44:03 +0000

    1111

--- GoalTracker2/Home/Components/HomeCircularGoalCell/GoalCircle/FlapScoreView/FlapScoreView.swift
+++ GoalTracker2/Home/Components/HomeCircularGoalCell/GoalCircle/FlapScoreView/FlapScoreView.swift
@@ -28,6 +28,54 @@ class FlapScoreView: UIView {
         return label
     }()
     
+    let goalCircularCollectionView = CircularCollectionView()
+    
+    let plusRotatingButton: NeumorphicButton = {
+        let button = NeumorphicButton(color: .crayon, shadowSize: .medium)
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
+    lazy var plusRotatingButtonInsideImageView: UIImageView = {
+        let imageView = UIImageView(imageName: "plus.neumorphism")
+        plusRotatingButton.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(22)
+        }
+        return imageView
+    }()
+
+    private let messageBar = MessageBar()
+    
+    private let topTransparentScreenView = UIView()
+    
+    private let bottomTransparentScreenView = UIView()
+    
+    private let topCalendarButton: NeumorphicButton = {
+        let button = NeumorphicButton(color: .crayon, shadowSize: .medium)
+        button.layer.cornerRadius = 18
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(named: "calendar.neumorphism")
+        button.configuration = configuration
+        return button
+    }()
+    
+    private let bottomDateCalendarButton: UIButton = {
+        var configuration = UIButton.Configuration.plain()
+        configuration.image = UIImage(named: "calendar.neumorphism")
+        configuration.imagePlacement = .leading
+        configuration.titleAlignment = .trailing
+        configuration.imagePadding = 6
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+        let button = UIButton()
+        button.configuration = configuration
+        button.backgroundColor = .crayon.withAlphaComponent(0.6)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    
     init() {
         super.init(frame: .zero)
         
